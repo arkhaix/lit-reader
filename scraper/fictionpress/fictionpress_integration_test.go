8a21a70de0f713349acc4ef83159f427aa7e4d74
Andrew Gray <github@xeisnet.com>
2018-08-16 13:15:36 +0800

    Added integration tests for fictionpress and royalroad
    - Each scraper has a single integration test just to make sure that everything parses correctly.  This is intended to inform me when sites change their layouts and the scrapers need to be adjusted.
    - Uses stretchr/testify for asserts.
    refs #1

---
+++ scraper/fictionpress/fictionpress_integration_test.go
@@ -0,0 +1,53 @@
+// +build integration
+
+package fictionpress_test
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	. "github.com/arkhaix/lit-reader/scraper/fictionpress"
+)
+
+var storyURL string
+
+func init() {
+	storyURL = "https://www.fictionpress.com/s/2922431/1/A-Lucky-Apocalypse"
+}
+
+func TestFictionPressIntegration(t *testing.T) {
+	s := NewScraper()
+	story, err := s.FetchStoryMetadata(storyURL)
+	assert.Nil(t, err, "Failed to fetch the story")
+
+	expectedURL := storyURL
+	expectedTitle := "A Lucky Apocalypse"
+	expectedAuthor := "ShaperV"
+	expectedChapters := 4
+
+	// Validate the story metadata
+	assert.Equal(t, expectedURL, story.URL, "URL must match")
+	assert.Equal(t, expectedTitle, story.Title, "Title must match")
+	assert.Equal(t, expectedAuthor, story.Author, "Author must match")
+	assert.Equal(t, expectedChapters, len(story.Chapters), "Number of chapters must match")
+
+	// Validate the data for a chapter
+	s.FetchChapter(&story, 0)
+	c := story.Chapters[0]
+	expectedChapterURL := "https://www.fictionpress.com/s/2922431/1/A-Lucky-Apocalypse"
+	expectedChapterTitle := "Chapter 1"
+	expectedTextSum := "9c8fa415b39ae1a863c0a82161360d89d7595ff900e608ac661aa74928d803bb"
+	expectedHTMLSum := "20ba190838a61d6ecfbdfaeb810366326220f6a2d9bb774955d9382060be888d"
+	textSum := sha256.Sum256([]byte(c.Text))
+	textSumStr := fmt.Sprintf("%x", textSum)
+	htmlSum := sha256.Sum256([]byte(c.HTML))
+	htmlSumStr := fmt.Sprintf("%x", htmlSum)
+
+	assert.Equal(t, expectedChapterURL, c.URL, "Chapter URL must match")
+	assert.Equal(t, expectedChapterTitle, c.Title, "Chapter title must match")
+	assert.Equal(t, expectedTextSum, textSumStr, "Chapter text must match")
+	assert.Equal(t, expectedHTMLSum, htmlSumStr, "Chapter HTML must match")
+}
---
+++ scraper/royalroad/royalroad_integration_test.go
@@ -0,0 +1,55 @@
+// +build integration
+
+package royalroad_test
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	. "github.com/arkhaix/lit-reader/scraper/royalroad"
+)
+
+var storyURL string
+
+func init() {
+	storyURL = "https://www.royalroad.com/fiction/15130/threadbare"
+}
+
+func TestRoyalRoadIntegration(t *testing.T) {
+	s := NewScraper()
+	story, err := s.FetchStoryMetadata(storyURL)
+	if err != nil {
+		t.Fatal("Failed to fetch the story", storyURL, err)
+	}
+
+	expectedURL := storyURL
+	expectedTitle := "Threadbare"
+	expectedAuthor := "Andrew Seiple"
+	expectedChapters := 78
+
+	// Validate the story metadata
+	assert.Equal(t, expectedURL, story.URL, "URL must match")
+	assert.Equal(t, expectedTitle, story.Title, "Title must match")
+	assert.Equal(t, expectedAuthor, story.Author, "Author must match")
+	assert.Equal(t, expectedChapters, len(story.Chapters), "Number of chapters must match")
+
+	// Validate the data for a chapter
+	s.FetchChapter(&story, 0)
+	c := story.Chapters[0]
+	expectedChapterURL := "https://www.royalroad.com/fiction/15130/threadbare/chapter/175199/awakening-1"
+	expectedChapterTitle := "Awakening 1"
+	expectedTextSum := "b232445b0ad233ff719d198da7588d3bdf3509dfeeba5816d9758ddf00f1bcec"
+	expectedHTMLSum := "2ab31d5b1b2052070978e8c413cbb5c4fb611f2daaa5d69de931e773a35a7e2c"
+	textSum := sha256.Sum256([]byte(c.Text))
+	textSumStr := fmt.Sprintf("%x", textSum)
+	htmlSum := sha256.Sum256([]byte(c.HTML))
+	htmlSumStr := fmt.Sprintf("%x", htmlSum)
+
+	assert.Equal(t, expectedChapterURL, c.URL, "Chapter URL must match")
+	assert.Equal(t, expectedChapterTitle, c.Title, "Chapter title must match")
+	assert.Equal(t, expectedTextSum, textSumStr, "Chapter text must match")
+	assert.Equal(t, expectedHTMLSum, htmlSumStr, "Chapter HTML must match")
+}
