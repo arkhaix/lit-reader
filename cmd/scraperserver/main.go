df01d9a444015c81c980cd7db52d90d3da907a5f
Andrew Gray <github@xeisnet.com>
2018-08-17 19:18:04 +0800

    Built a gRPC service around the scraper
    - cmd/scraperserver is a simple gRPC server that exposes the scrapers.  It's adapted from the gRPC helloworld example.  I've been using grpc_cli to test it, so I haven't written an example client yet.
    - The api is slightly different from the existing scraper interface, and I think I'll be refactoring the old stuff to match it (or at least better support it).
    - refs #2

---
+++ cmd/scraperserver/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	api "github.com/arkhaix/lit-reader/api/scraper"
+	server "github.com/arkhaix/lit-reader/servers/scraper"
+)
+
+const (
+	port = ":50051"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	api.RegisterScraperServer(s, &server.Server{})
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
