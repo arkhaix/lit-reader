2432d4a76c8d7e2d7536029e209106ec6342204c
Unknown <github@xeisnet.com>
2018-08-22 18:47:18 +0800

    Built kubernetes deployment for the story service
    - Tested on gke.
    - This still doesn't have any persistence yet, but I wanted to get this easier config done first for the sake of having a solid starting point.
    - Added a convenience script for building and pushing the docker images to gke.  It's targeted at my personal project right now.  I might move it to docker hub later.
    - refs #11

---
+++ docker-all-gke.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+docker build -t scraper-grpc -f ./build/servers/grpc/scraper.Dockerfile .
+docker build -t story-grpc -f ./build/servers/grpc/story.Dockerfile .
+docker build -t story-http -f ./build/servers/http/story.Dockerfile .
+docker build -t storytest-http -f ./build/servers/http/storytest.Dockerfile .
+
+docker tag scraper-grpc gcr.io/arkhaix-lit-reader/scraper-grpc
+docker tag story-grpc gcr.io/arkhaix-lit-reader/story-grpc
+docker tag story-http gcr.io/arkhaix-lit-reader/story-http
+docker tag storytest-http gcr.io/arkhaix-lit-reader/storytest-http
+
+docker push gcr.io/arkhaix-lit-reader/scraper-grpc
+docker push gcr.io/arkhaix-lit-reader/story-grpc
+docker push gcr.io/arkhaix-lit-reader/story-http
+docker push gcr.io/arkhaix-lit-reader/storytest-http
